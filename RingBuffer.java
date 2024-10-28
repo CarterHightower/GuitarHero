4cd8326f600917d1661631ecf737d80f628a94d7
CarterHightower <hightowercarter10@gmail.com>
2024-10-28 14:25:56 -0500

    Add files via upload

---
+++ RingBuffer.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class RingBuffer {
+	private int cap;
+	public RingBuffer(int capacity){
+		cap = capacity;
+	}
+	public int size() {
+		
+	}
+	public boolean isEmpty() {
+		
+	}
+	public boolean isFull() {
+		
+	}
+	public void enqueue(double x) {
+		
+	}
+	public double dequeue() {
+		
+	}
+	public double peek() {
+		
+	}
+	public String toString() {
+		
+	}
+	
+}
